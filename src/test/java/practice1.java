1768f08f48175a878f95648c46e0685e2509fc9d
SergiuBodea <serg.bodea@gmail.com>
2021-06-12 21:32:30 +0300

    doing more work and publsih on Github

--- src/test/java/practice1.java
+++ src/test/java/practice1.java
@@ -1,9 +1,19 @@
 public class practice1 {
+
+
+
     public static void main(String[] args) {
 
         System.out.println("Eu is liking Git on InteliJ");
 
         System.out.println("Work1");
 
+        //succesfully pushed project to Github
+            //first intelliJ automatically created remote page
+        //created connection between local repo and remote repo
+            //pushed local commits to remote repo
+
     }
+
+
 }
